ad884769c0ff94d8e4b91d0cafd6a6b28f31f852
Ali Farahmand <alifarahmand1993@gmail.com>
2023-12-10 16:08:19 -0500

    Add HeadLineCards Component to Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
 import Hero from "./Component/Hero";
+import HeadLineCards from "./Component/HeadLineCards";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadLineCards />
     </div>
   );
 }
