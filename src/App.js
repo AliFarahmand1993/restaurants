cc30ed2efb1e654772e403af99fbde449beaf21b
Ali Farahmand <alifarahmand1993@gmail.com>
2023-12-12 14:06:51 -0500

    add footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./Component/Hero";
 import HeadLineCards from "./Component/HeadLineCards";
 import Foods from "./Component/Foods";
 import Category from "./Component/Category";
+import Footer from "./Component/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeadLineCards />
       <Foods />
       <Category />
+      <Footer />
     </div>
   );
 }
